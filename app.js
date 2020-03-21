30440e574d7d2066edd480b55cd30cb0bb08a2bb
Meet Usadadiya <meet123.usadadiya@gmail.com>
2020-03-21 21:26:32 +0530

    11111111111

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
-const express = require('express');
-const app = express();
+const fs = require('fs');
 require('dotenv').config();
-
+let arr = require('./list').arr;
 var AWS = require('aws-sdk');
 
-app.get('/', (req, res) => {
+let i = 0,
+	j = arr.length;
+// req.query.message
+let bodymsg = `As per Govt. of Gujarat's notification regarding COVID-19, SPECTRUM 2020 & H2O RUN have been postponed until further announcement. Stay Tuned!\n\nHealth is above everything!\n\nContact: +919879212015 for any queries.\n\nRegards,\nTeam SPECTRUM 2020, ADIT`;
 
-    console.log("Message = " + req.query.message);
-    console.log("Number = " + req.query.number);
-    console.log("Subject = " + req.query.subject);
-    var params = {
-        Message: req.query.message,
-        PhoneNumber: '+' + req.query.number,
-        MessageAttributes: {
-            'AWS.SNS.SMS.SenderID': {
-                'DataType': 'String',
-                'StringValue': req.query.subject
-            }
-        }
-    };
+setInterval(() => {
+	if (i == j) {
+		console.log('Completed');
 
-    var publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
+		process.exit(1);
+	}
+	var params = {
+		Message: bodymsg,
+		PhoneNumber: '+91' + arr[i]
+	};
+	var publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' })
+		.publish(params)
+		.promise();
+	publishTextPromise
+		.then(function(data) {
+			console.log({
+				mobile: params.PhoneNumber,
+				MessageID: data.MessageId
+			});
+		})
+		.catch(function(err) {
+			err = err.toString();
+			fs.appendFileSync(
+				'./failed.txt',
+				JSON.stringify({
+					err: err,
+					mob: params.PhoneNumber
+				})
+			);
+			console.log({ mobile: params.PhoneNumber, Error: err });
+		});
+	i++;
+}, 8000);
 
-    publishTextPromise.then(
-        function (data) {
-            res.end(JSON.stringify({ MessageID: data.MessageId }));
-        }).catch(
-            function (err) {
-                res.end(JSON.stringify({ Error: err }));
-            });
-
-});
-
-app.listen(3000, () => console.log('SMS Service Listening on PORT 3000'))
+// new AWS.SNS().setSMSAttributes({});
